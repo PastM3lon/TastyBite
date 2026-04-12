c6fa274b811060bac399adce2068f89602e61866
PastM3lon <omaralshora1@gmail.com>
2026-04-12 13:12:23 +0200

    nav bar fix

--- Recipes Page/recipes.js
+++ Recipes Page/recipes.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
    const isAdmin = localStorage.getItem("isAdmin") === "true";
-   const adminNavItem = document.querySelector(".admin-icon");
+   const adminNavItem = document.querySelector(".admin-link");
 
    if (adminNavItem && !isAdmin) {
       adminNavItem.style.display = "none";
