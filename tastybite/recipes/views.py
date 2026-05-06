8627b958cb2f0af593ac02e384e4f1934ea02755
Adham-mostafa-hub <mostafaadham29@gmail.com>
2026-05-06 07:55:43 +0300

    Add multiple view functions for recipe app

--- tastybite/recipes/views.py
+++ tastybite/recipes/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+
+def welcome_view(request):
+    return render(request, 'welcome.html')
+
+
+def recipes_list(request):
+    return render(request, 'recipes_list.html')
+
+
+def favorites_view(request):
+    return render(request, 'favorites.html')
+
+
+def search_view(request):
+    return render(request, 'search.html')
+
+
+def dishes_view(request):
+    return render(request, 'dishes.html')
+
+
+def auth_view(request):
+    return render(request, 'auth.html')
